0f5586ac49cc99ea407f03fe40b3f83034ea20df
Diego <Diego@DESKTOP-E77NVCU>
2022-04-24 16:13:53 -0300

    Adicionar arquivos de projeto.

---
+++ ImportDbPNS/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace ImportDbPNS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelecionar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMicroDados = new System.Windows.Forms.TextBox();
+            this.txtArqEstrutural = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnArqEstrutural = new System.Windows.Forms.Button();
+            this.lblValidaEstrutura = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtNomeclatura = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnNomeclatura = new System.Windows.Forms.Button();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSenhaBD = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSelecionar
+            // 
+            this.btnSelecionar.Location = new System.Drawing.Point(12, 58);
+            this.btnSelecionar.Name = "btnSelecionar";
+            this.btnSelecionar.Size = new System.Drawing.Size(75, 23);
+            this.btnSelecionar.TabIndex = 2;
+            this.btnSelecionar.Text = "Seleciar";
+            this.btnSelecionar.UseVisualStyleBackColor = true;
+            this.btnSelecionar.Click += new System.EventHandler(this.btnSelecionar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Selecione Arquivo do Microdados .txt";
+            // 
+            // txtMicroDados
+            // 
+            this.txtMicroDados.Location = new System.Drawing.Point(12, 30);
+            this.txtMicroDados.Name = "txtMicroDados";
+            this.txtMicroDados.Size = new System.Drawing.Size(196, 20);
+            this.txtMicroDados.TabIndex = 1;
+            this.txtMicroDados.TabStop = false;
+            // 
+            // txtArqEstrutural
+            // 
+            this.txtArqEstrutural.Location = new System.Drawing.Point(12, 121);
+            this.txtArqEstrutural.Name = "txtArqEstrutural";
+            this.txtArqEstrutural.Size = new System.Drawing.Size(196, 20);
+            this.txtArqEstrutural.TabIndex = 2;
+            this.txtArqEstrutural.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Selecione Arquivo Extrutural .xls";
+            // 
+            // btnArqEstrutural
+            // 
+            this.btnArqEstrutural.Location = new System.Drawing.Point(12, 149);
+            this.btnArqEstrutural.Name = "btnArqEstrutural";
+            this.btnArqEstrutural.Size = new System.Drawing.Size(75, 23);
+            this.btnArqEstrutural.TabIndex = 3;
+            this.btnArqEstrutural.Text = "Carregar";
+            this.btnArqEstrutural.UseVisualStyleBackColor = true;
+            this.btnArqEstrutural.Click += new System.EventHandler(this.btnArqEstrutural_Click);
+            // 
+            // lblValidaEstrutura
+            // 
+            this.lblValidaEstrutura.AutoSize = true;
+            this.lblValidaEstrutura.Location = new System.Drawing.Point(228, 125);
+            this.lblValidaEstrutura.Name = "lblValidaEstrutura";
+            this.lblValidaEstrutura.Size = new System.Drawing.Size(65, 13);
+            this.lblValidaEstrutura.TabIndex = 6;
+            this.lblValidaEstrutura.Text = "Label Valida";
+            this.lblValidaEstrutura.Visible = false;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(361, 225);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 5;
+            this.btnInsert.Text = "Realizar Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(382, 274);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 8;
+            // 
+            // txtNomeclatura
+            // 
+            this.txtNomeclatura.Location = new System.Drawing.Point(12, 246);
+            this.txtNomeclatura.Name = "txtNomeclatura";
+            this.txtNomeclatura.Size = new System.Drawing.Size(196, 20);
+            this.txtNomeclatura.TabIndex = 11;
+            this.txtNomeclatura.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Importar Nomeclatura .xls";
+            // 
+            // btnNomeclatura
+            // 
+            this.btnNomeclatura.Location = new System.Drawing.Point(12, 274);
+            this.btnNomeclatura.Name = "btnNomeclatura";
+            this.btnNomeclatura.Size = new System.Drawing.Size(75, 23);
+            this.btnNomeclatura.TabIndex = 4;
+            this.btnNomeclatura.Text = "Carregar";
+            this.btnNomeclatura.UseVisualStyleBackColor = true;
+            this.btnNomeclatura.Click += new System.EventHandler(this.btnNomeclatura_Click);
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(361, 30);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(133, 20);
+            this.txtEndereco.TabIndex = 0;
+            this.txtEndereco.Text = "192.168.10.51,3739";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(361, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Endereço BD SQL";
+            // 
+            // txtSenhaBD
+            // 
+            this.txtSenhaBD.Location = new System.Drawing.Point(361, 93);
+            this.txtSenhaBD.Name = "txtSenhaBD";
+            this.txtSenhaBD.PasswordChar = '*';
+            this.txtSenhaBD.Size = new System.Drawing.Size(133, 20);
+            this.txtSenhaBD.TabIndex = 1;
+            this.txtSenhaBD.Text = "s";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(361, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Senha BD";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 323);
+            this.Controls.Add(this.txtSenhaBD);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNomeclatura);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnNomeclatura);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.lblValidaEstrutura);
+            this.Controls.Add(this.txtArqEstrutural);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnArqEstrutural);
+            this.Controls.Add(this.txtMicroDados);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSelecionar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelecionar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMicroDados;
+        private System.Windows.Forms.TextBox txtArqEstrutural;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnArqEstrutural;
+        private System.Windows.Forms.Label lblValidaEstrutura;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtNomeclatura;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnNomeclatura;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSenhaBD;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ImportDbPNS/Form1.cs
@@ -0,0 +1,404 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using OfficeOpenXml;
+using System.IO;
+using ExcelLibrary.SpreadSheet;
+using System.Data.SqlClient;
+
+namespace ImportDbPNS
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+
+        OpenFileDialog op;
+        string Dir;
+        Dictionary<string, List<Coluna>> DbPnsDictrionary;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnSelecionar_Click(object sender, EventArgs e)
+        {
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                txtMicroDados.Text = op.FileName;
+                Dir = op.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            op = new OpenFileDialog();
+            DbPnsDictrionary = new Dictionary<string, List<Coluna>>();
+        }
+
+        private void btnArqEstrutural_Click(object sender, EventArgs e)
+        {
+            string arq;
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                txtArqEstrutural.Text = op.FileName;
+                arq = op.FileName;
+
+                var a = Workbook.Load(arq);
+                var workSheet = a.Worksheets.First();
+
+                try
+                {
+                    //Percorrendo as linhas  a partir da segunda linha - 1
+                    for (int i = 1; i <= workSheet.Cells.LastRowIndex; i++)
+                    {
+                        string tab = workSheet.Cells[i, 0].ToString();
+                        string b = workSheet.Cells[i, 1].ToString();
+                        string b1 = workSheet.Cells[i, 2].ToString();
+
+                        Coluna c = new Coluna() { Nome = workSheet.Cells[i, 1].ToString(), Posicao = workSheet.Cells[i, 2].ToString(), Tamanho = workSheet.Cells[i, 3].ToString() };
+
+                        if (!DbPnsDictrionary.Keys.Contains(tab))
+                        {
+                            List<Coluna> lstCol = new List<Coluna>();
+                            DbPnsDictrionary.Add(tab, lstCol);
+                        }
+                        DbPnsDictrionary[tab].Add(c);
+                    }
+                    lblValidaEstrutura.Text = "Importado com Sucesso!!!";
+                    lblValidaEstrutura.BackColor = Color.Green;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao carregar o arquivo. \nDetalhes: " + ex.Message);
+                    lblValidaEstrutura.Text = "Erro ao importar arquivo!";
+                    lblValidaEstrutura.BackColor = Color.Red;
+                }
+                finally
+                {
+                    
+                    lblValidaEstrutura.Visible = true;
+                }
+            }
+        }
+
+        #region teste de Leitura e criação do Insert errada***
+        //private void btnInsert_Click(object sender, EventArgs e)
+        //{
+        //    int ContLinhas = 1;
+        //    using (StreamReader sr = new StreamReader(Dir))
+        //    {
+
+        //        string line = string.Empty;
+
+        //        lstDados = new List<Nov[]>();
+
+
+        //        while ((line = sr.ReadLine()) != null)
+        //        {
+        //            foreach (var item in DbPnsDictrionary.Keys)
+        //            {
+        //                //criando array para receber variavel(Coluna e seu valor retornado do Arquivo)
+        //                cc = new Nov[DbPnsDictrionary[item].Count()];
+
+        //                //Listando todas colunas de tabela especifica "item" e incluindo no Array 
+        //                for (int i = 0; i < DbPnsDictrionary[item].Count; i++)
+        //                {
+        //                    int pos = Convert.ToInt32(DbPnsDictrionary[item][i].Posicao);
+        //                    int tam = Convert.ToInt32(DbPnsDictrionary[item][i].Tamanho);
+
+        //                    cc[i] = new Nov();
+        //                    cc[i].Nome = DbPnsDictrionary[item][i].Nome;
+
+        //                    string bbb = line.Substring(200, 8);
+        //                    cc[i].Valor = line.Substring(pos-1, tam);
+        //                }
+        //                lstDados.Add(cc);
+        //                //Dados a serem trabalhados no insert
+        //            }
+        //            if(ContLinhas == 20)
+        //            {
+        //                //fazer inserção e limpar listas
+        //                string str = "insert into PNS..{0}  ( ";
+        //                string[] a;
+        //                string[] b;
+        //                int indx;
+        //                int qq;
+        //                foreach (var item in DbPnsDictrionary.Keys)
+        //                {
+        //                    int tam = DbPnsDictrionary[item].Count();
+        //                    indx= 0;
+        //                    a = new string[tam];
+        //                    b = new string[tam];
+        //                    for (int i = 0; i < tam; i++)
+        //                    {
+        //                        a[i] = "('";
+        //                        b[i] = "('";
+        //                    }
+
+        //                    qq = tam;
+        //                    Dictionary<string, int> dic = new Dictionary<string, int>();
+        //                    foreach (var i in DbPnsDictrionary[item])
+        //                    {
+        //                        Nov n = new Nov();
+        //                        n = lstDados[0].Where(z => z.Nome == i.Nome).FirstOrDefault();
+
+        //                        if (!dic.Keys.Contains(i.Nome))
+        //                        {
+        //                            a[indx] = a[indx] + i.Nome + ", ";
+        //                            dic.Add(i.Nome, indx);
+
+        //                            b[indx] = b[indx] + n.Valor + "', '";
+        //                        }
+        //                        else
+        //                            b[dic[i.Nome]] = b[dic[i.Nome]] + n.Valor + "', '";
+
+        //                        indx++;
+        //                    }
+        //                    for (int i = 0; i < indx; i++)
+        //                    {
+        //                        a[i] = a[i].Remove(a.Length - 2, 2);
+        //                        a[i] = a[i] + ") ";
+        //                        b[i] = b[i].Remove(b.Length - 3, 3);
+        //                        b[i] = b[i] + ") ";
+        //                    }
+
+        //                    lstDados.Remove(lstDados[0]);
+
+
+        //                }
+        //                for (int i = 0; i < a[].Count(); i++)
+        //                {
+        //                    a[i] = a[i].Remove(a.Length - 2, 2);
+        //                    a[i] = a[i] + ") ";
+        //                    b[i] = b[i].Remove(b[i].Length - 2, 2);
+        //                    b[i] = b[i] + ";";
+        //                }
+        //            }
+
+        //            ContLinhas++;
+        //        }
+
+        //    }
+        //}
+        #endregion
+
+        bool result = true;
+        private async void btnInsert_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection($"Data Source = {txtEndereco.Text}; Initial Catalog = PNS; User Id = sa; Password = {txtSenhaBD.Text}"); //Cria a conexão com o Banco de dados devido a Dll que tem na pasta do programa.
+
+            conn.Open();
+            await teste1();
+            conn.Close();
+        }
+        private Task teste1()
+        {
+            
+
+            return Task.Factory.StartNew(() =>
+            {
+                foreach (var item in DbPnsDictrionary.Keys)
+                {
+
+                    using (StreamReader sr = new StreamReader(Dir))
+                    {
+                        string line = string.Empty;
+
+                        int ContLinhas = 0;
+                        int ContTotal = 0;
+                        string str = "insert into PNS.." + item;
+
+                        string create_Table = "Create Table PNS.." + item + " (Id int, ";
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            string a = " (Id, ";
+                            string b = " (" + (ContTotal + 1) + ", '";
+
+                            for (int i = 0; i < DbPnsDictrionary[item].Count; i++)
+                            {
+                                int pos = Convert.ToInt32(DbPnsDictrionary[item][i].Posicao);
+                                int tam = Convert.ToInt32(DbPnsDictrionary[item][i].Tamanho);
+
+                                //cc[i] = new Nov();
+                                //cc[i].Nome = DbPnsDictrionary[item][i].Nome;
+                                //cc[i].Valor = line.Substring(pos - 1, tam);
+
+                                if (ContLinhas == 0)
+                                    a = a + DbPnsDictrionary[item][i].Nome + ", ";
+
+                                if (ContTotal == 0)
+                                    create_Table = create_Table + DbPnsDictrionary[item][i].Nome + " varchar(" + tam + "), ";
+
+                                b = b + line.Substring(pos - 1, tam) + "', '";
+                            }
+                            a = a.Remove(a.Length - 2, 2);
+                            a = a + ")";
+                            b = b.Remove(b.Length - 3, 3);
+                            b = b + "),";
+
+
+                            if (ContLinhas == 0)
+                                str = str + a + " values";
+                            str = str + b;
+
+                            if(ContTotal ==0)
+                            {
+                                create_Table = create_Table.Remove(create_Table.Length - 2, 2);
+                                create_Table += ");";
+                                string resp = ExecuteInsertDB(create_Table);
+                                if (resp.Count() > 10)
+                                {
+                                    var resposta = MessageBox.Show(resp + " \nDeseja limpar a tabela?", "Atenção", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                                    if (resposta == DialogResult.Yes)
+                                    {
+                                        string trunc = "truncate table PNS.." + item + ";";
+                                        ExecuteInsertDB(trunc);
+                                    }
+                                }
+                            }
+
+
+                            ContLinhas++;
+                            ContTotal++;
+                            int altQtd = str.Length;
+                            if (str.Length >= 8000)
+                            {
+                                
+                                ExecuteInsertDB(str);
+
+                                ContLinhas = 0;
+                                str = "insert into PNS.." + item;
+
+
+                                lblResult.Invoke(new MethodInvoker(() => lblResult.Text = string.Format("Importando tabela:{0}, \nQTD de linhas Alteradas:{1}", item, ContTotal)));
+                            }
+                        }
+                        if (ContLinhas > 1)
+                            ExecuteInsertDB(str);
+
+                    }
+
+
+                }
+            });
+        }
+        
+
+        private string ExecuteInsertDB(string SQL_Insert)
+        {
+            try
+            {
+                SQL_Insert = SQL_Insert.Remove(SQL_Insert.Length - 1, 1);
+                SQL_Insert = SQL_Insert + ";";
+
+                //SqlConnection conn = new SqlConnection("Data Source = 192.168.10.51,3739; Initial Catalog = PNS; User Id = sa; Password = d120588$788455"); //Cria a conexão com o Banco de dados devido a Dll que tem na pasta do programa.
+                SqlCommand cmd = new SqlCommand(SQL_Insert);
+
+                //conn.Open();
+                cmd.Connection = conn;
+                int r = cmd.ExecuteNonQuery();
+                //conn.Close();
+                return r.ToString();
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        private void btnNomeclatura_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection($"Data Source = {txtEndereco.Text}; User Id = sa; Password ={txtSenhaBD.Text}"); //Cria a conexão com o Banco de dados devido a Dll que tem na pasta do programa.
+
+                conn.Open();
+                string createBD = "drop database PNS;create database PNS;";
+                ExecuteInsertDB(createBD);
+
+                string arq;
+                if (op.ShowDialog() == DialogResult.OK)
+                {
+                    txtNomeclatura.Text = op.FileName;
+                    arq = op.FileName;
+
+                    var a = Workbook.Load(arq);
+                    var workSheet = a.Worksheets.First();
+
+                    List<Nov> lstNomeclatura = new List<Nov>();
+                    string str = "";
+                    for (int i = 0; i <= workSheet.Cells.LastRowIndex; i++)
+                    {
+
+                        Nov c = new Nov() { Tipo = workSheet.Cells[i, 0].ToString(), Nome = workSheet.Cells[i, 1].ToString(), Valor = workSheet.Cells[i, 2].ToString() };
+                        lstNomeclatura.Add(c);
+                        if (i == 0)
+                        {
+                            string st = "create table PNS.._Enumerador (Tipo varchar(20), Valor varchar(100), Descricao varchar(1000));";
+                            string result = "";
+                            result = ExecuteInsertDB(st);
+                            if (result.Count() > 10)
+                            {
+                                var resp = MessageBox.Show("Já existe a tabela criada, deseja Limpa-lá?", "Deseja limpar a tabela?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                                if (resp == DialogResult.Yes)
+                                {
+                                    ExecuteInsertDB("truncate table PNS.._Enumerador;");
+                                }
+                            }
+
+
+
+                        }
+                        if (lstNomeclatura.Count() == 200)
+                        {
+                            str = "insert into PNS.._Enumerador (Tipo, Valor, Descricao) Values ('";
+
+                            foreach (var item in lstNomeclatura)
+                            {
+                                str = str + item.Tipo + "', '" + item.Nome + "', '" + item.Valor + "'), ('";
+                            }
+                            str = str.Remove(str.Length - 4, 4);
+                            str = str + ";";
+                            string res = "";
+                            res = ExecuteInsertDB(str);
+                            lstNomeclatura.Clear();
+                        }
+
+
+                    }
+                    if (lstNomeclatura.Count() > 1)
+                    {
+                        str = "insert into PNS.._Enumerador (Tipo, Valor, Descricao) Values ('";
+
+                        foreach (var item in lstNomeclatura)
+                        {
+                            str = str + item.Tipo + "', '" + item.Nome + "', '" + item.Valor + "'), ('";
+                        }
+                        str = str.Remove(str.Length - 4, 4);
+                        str = str + ";";
+                        string r = "";
+                        r = ExecuteInsertDB(str);
+                        lstNomeclatura.Clear();
+
+
+                    }
+                }
+            }
+            catch { MessageBox.Show("Erro ao importar arquivo, tente novamente."); }
+            finally { conn.Close(); }
+        }
+
+       
+    }
+
+}
